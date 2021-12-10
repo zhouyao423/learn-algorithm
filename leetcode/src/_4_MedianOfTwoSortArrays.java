6fbaa6da16abd57158aae6b49e120fd765aa4583
zhouyao <yao.zhou@vcg.com>
2021-12-10 11:25:55 +0800

    递归方法解有序数组中位数

--- leetcode/src/_4_MedianOfTwoSortArrays.java
+++ leetcode/src/_4_MedianOfTwoSortArrays.java
@@ -27,6 +27,7 @@ public class _4_MedianOfTwoSortArrays {
         System.out.println(binarySearch(new int[]{0, 1, 5, 6, 7, 9, 10}, 5));
 
         System.out.println(findMedian(new int[]{1}, new int[]{1}));
+        System.out.println(recursiveFind(new int[]{}, new int[]{ 4}));
     }
 
     //不用二分法找中位数
@@ -120,6 +121,44 @@ public class _4_MedianOfTwoSortArrays {
         }
     }
 
+    //递归查询
+    private static float recursiveFind(int[] a, int[] b) {
+        //找两个有序数组中的第K大的数（数量为奇数需要找第K大，数量为偶数需要找第K和第K+1大）
+        //在两个数组中先找 0 到K/2个数字，直到K/2为0
+        int totalLength = a.length + b.length;
+        int k = totalLength / 2;
+        int x = k == 1 ? 1 : k / 2;
+        if ((totalLength & 1) == 0) {
+            return (recursive(a, 0, b, 0, x, k - 1) + recursive(a, 0, b, 0, x, k)) / 2.0f;
+        }
+        return recursive(a, 0, b, 0, x, k);
+    }
+
+    // x 表示两个数组中要向后移动的个数
+    private static int recursive(int[] a, int aStart, int[] b, int bStart, int x, int k) {
+        if (k == 0) {
+            x = 0;
+        }
+        //边界减枝
+        if (aStart == a.length) {
+            return b[bStart + k];
+        }
+        if (bStart == b.length) {
+            return a[aStart + k];
+        }
+        if (x == 0) {
+            return Math.min(a[aStart], b[bStart]);
+        }
+        //偏序关系
+        if (aStart + x <= a.length && (bStart + x > b.length || a[aStart + x - 1] < b[bStart + x - 1])) {
+            aStart += x;
+        } else {
+            bStart += x;
+        }
+        k = k - x;
+        x = k == 1 ? 1 : k / 2;
+        return recursive(a, aStart, b, bStart, x, k);
+    }
 
     //二分查找的时间复杂度为O(log2n)
     //先复习一下二分查找算法 寻找一个数x
