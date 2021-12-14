cd038fbb5c1bfd92f65819bcfb3891be19f64d40
zhouyao <yao.zhou@vcg.com>
2021-12-14 14:13:55 +0800

    leetcode 序号5  最长回文数，动态规划解法

--- DynamicProgramming/src/_3_LCS_01.java
+++ DynamicProgramming/src/_3_LCS_01.java
@@ -6,7 +6,8 @@
  **/
 public class _3_LCS_01 {
     public static void main(String[] args) {
-        System.out.println(lcs1("acdaccceaaadcafaab", "aacada"));
+        System.out.println(lcs1("acdaccceaaadcafaab", "aacadca"));
+        System.out.println(lcs2("acdaccceaaadcafaab", "aacadca"));
     }
 
     private static int lcs1(String x, String y) {
@@ -39,6 +40,28 @@ public class _3_LCS_01 {
             }
         }
         System.out.println(x.substring(xLastIndex - max, xLastIndex));
+        return max;
+    }
+
+    //最长公共子串
+    private static int lcs2(String x, String y) {
+        char[] xArray = x.toCharArray();
+        char[] yArray = y.toCharArray();
+        int max = 0;
+        //用一个数组保存上一步计算的公共字符串长度
+        int[][] tempArray = new int[xArray.length][yArray.length];
+        for (int i = 0; i < xArray.length; i++) {
+            for (int j = 0; j < yArray.length; j++) {
+                if (i == 0 || j == 0) {
+                    tempArray[i][j] = xArray[i] == yArray[j] ? 1 : 0;
+                } else {
+                    if (xArray[i] == yArray[j]) {
+                        tempArray[i][j] = tempArray[i - 1][j - 1] + 1;
+                    }
+                }
+                max = Math.max(max, tempArray[i][j]);
+            }
+        }
         return max;
     }
 }
---
+++ leetcode/src/_5_LongestPalindrome.java
@@ -0,0 +1,96 @@
+public class _5_LongestPalindrome {
+
+//    给你一个字符串 s，找到 s 中最长的回文子串。
+//    示例 1：
+//    输入：s = "babad"
+//    输出："bab"
+//    解释："aba" 同样是符合题意的答案。
+//    示例 2：
+//    输入：s = "cbbd"
+//    输出："bb"
+//    示例 3：
+//    输入：s = "a"
+//    输出："a"
+//    示例 4：
+//
+//    输入：s = "ac"
+//    输出："a"
+//    提示：
+//
+//            1 <= s.length <= 1000
+//    s 仅由数字和英文字母（大写和/或小写）组成
+//
+//    来源：力扣（LeetCode）
+//    链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+//    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+//
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome1("ccc"));
+    }
+
+    //动态规划
+    private static String longestPalindrome1(String x) {
+        char[] xArray = x.toCharArray();
+        int length = xArray.length;
+        int max = 1;
+        int maxLeftIdx = 0;
+        boolean[][] dp = new boolean[length][length];
+        //填充dp表
+        for (int i = 0; i < xArray.length; i++) {
+            dp[i][i] = true;
+            if (i < xArray.length - 1) {
+                if (xArray[i] == xArray[i + 1]) {
+                    dp[i][i+1] = true;
+                    max = 2;
+                    maxLeftIdx = i;
+                }
+            }
+        }
+
+        for (int i = 2; i < length; i++) {
+            for (int j = 0; j + i < xArray.length; j++) {
+                if (dp[j + 1][j + i - 1] && (dp[j][j + i] = xArray[j] == xArray[j + i])) {
+                    if (i + 1 > max) {
+                        max = i + 1;
+                        maxLeftIdx = j;
+                    }
+                }
+            }
+        }
+        return x.substring(maxLeftIdx, maxLeftIdx + max);
+    }
+
+    //暴力解法
+    private static String longestPalindrome(String x) {
+
+        int max = 1;
+        int lIndex = 0;
+        char[] xArray = x.toCharArray();
+
+        for (int i = 0; i < xArray.length; i++) {
+            for (int j = i + max; j < xArray.length; j++) {
+                if (xArray[i] == xArray[j] && checkPalindrome(xArray, i, j)) {
+                    int len = j - i + 1;
+                    if (len > max) {
+                        max = len;
+                        lIndex = i;
+                    }
+                }
+            }
+        }
+        return x.substring(lIndex, lIndex + max);
+    }
+
+    private static boolean checkPalindrome(char[] xArray, int i, int j) {
+        while (i < j) {
+            if (xArray[i] != xArray[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
