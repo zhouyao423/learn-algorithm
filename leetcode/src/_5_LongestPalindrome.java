7c7fbaf070ee6ca5440fce781d47caf81e742e25
zhouyao <zhouyao423@gmail.com>
2021-12-15 14:03:04 +0800

    leetcode[5]:manacher

--- leetcode/src/_5_LongestPalindrome.java
+++ leetcode/src/_5_LongestPalindrome.java
@@ -27,16 +27,64 @@ public class _5_LongestPalindrome {
 
     public static void main(String[] args) {
         System.out.println(longestPalindrome1("ccc"));
+        System.out.println(manacher("cbbd"));
     }
+
     //manacher算法
-    private static String manacher(String x){
+    private static String manacher(String x) {
+        char[] array = init(x);
+        //声明数组pi用来保存索引为i的回文半径
+        int[] P = new int[array.length];
+        //当前回文半径
+        int R = 0;
+        //当前回文的中心下标
+        int C = 0;
+        for (int i = 1; i < array.length - 1; i++) {
+            // 当i在右边界以内，P[i] 至少等于i关于C的对称点
+            int right = C + R - 1;
+            if (i <= right) {
+                P[i] = Math.min(P[2 * C - i], right - i + 1);
+            }
+            //对i进行扩展
+            while (array[i + P[i]] == array[i - P[i]]) {
+                P[i]++;
+            }
+            //当右边界超出C+R-1,重置C和R
+            if (i + P[i] - 1 > right) {
+                C = i;
+                R = P[i];
+            }
+        }
 
-        return null;
+        int max = -1;
+        //计算P[i] 的最大值
+        for (int i = 0; i < P.length; i++) {
+            if (P[i] > max) {
+                max = P[i];
+                C = i;
+            }
+        }
+        //现字符串的左边界,左边界为#
+        int left = C - max + 1;
+        //原字符串的左边界
+        int originLeft = (left - 3) / 2 + 1;
+        //原字符串的长度
+        int length = (2 * max - 1) / 2;
+        return x.substring(originLeft, originLeft + length);
     }
-    //初始化数组 使用#在字符串中间填充 头尾使用^和$ 终止遍历
-    private static char[] init(){
 
-        return null;
+    //初始化数组 使用#在字符串中间填充 头尾使用^和$ 终止遍历
+    private static char[] init(String s) {
+        char[] chars = s.toCharArray();
+        char[] result = new char[chars.length * 2 + 3];
+        result[0] = '^';
+        result[1] = '#';
+        result[result.length - 1] = '$';
+        for (int i = 0; i < chars.length; i++) {
+            result[2 * i + 2] = chars[i];
+            result[2 * i + 3] = '#';
+        }
+        return result;
     }
 
     //动态规划
@@ -51,7 +99,7 @@ public class _5_LongestPalindrome {
             dp[i][i] = true;
             if (i < xArray.length - 1) {
                 if (xArray[i] == xArray[i + 1]) {
-                    dp[i][i+1] = true;
+                    dp[i][i + 1] = true;
                     max = 2;
                     maxLeftIdx = i;
                 }
