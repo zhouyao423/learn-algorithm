cddc0088707078586f7e99f054bce3eee8fd0b19
zhouyao <zhouyao423@gmail.com>
2022-02-09 14:46:01 +0800

    leetcode[322]:硬币兑换 记忆化搜索 + 动归

--- leetcode/src/_322_coinChange.java
+++ leetcode/src/_322_coinChange.java
@@ -1,4 +1,6 @@
+import javax.crypto.MacSpi;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class _322_coinChange {
     //322. 零钱兑换
@@ -17,42 +19,67 @@ public class _322_coinChange {
     //解释：11 = 5 + 5 + 1
     public static void main(String[] args) {
         _322_coinChange coinChange = new _322_coinChange();
-        coinChange.coinChange(new int[]{186,419,83,408},6249);
-        System.out.println();
+        int i = coinChange.coinChange(new int[]{2}, 3);
+        System.out.println(i);
     }
 
-    public int coinChange(int[] coins, int amount) {
-        if (amount == 0 ){
+    int[] memo;
+
+    public int coinChange1(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+        memo = new int[amount];
+
+        return findWay(coins, amount);
+    }
+
+    // memo[n] 表示钱币n可以被换取的最少的硬币数，不能换取就为-1
+    // findWay函数的目的是为了找到 amount数量的零钱可以兑换的最少硬币数量，返回其值int
+    public int findWay(int[] coins, int amount) {
+        if (amount < 0) {
+            return -1;
+        }
+        if (amount == 0) {
             return 0;
         }
-        Arrays.sort(coins);
-        int minDepth = Integer.MAX_VALUE;
-        for (int i = coins.length-1; i >=0 ; i--) {
-            int depth = dfs(coins, i, amount, 0);
-            if (depth >0) {
-                minDepth = Math.min(minDepth,depth);
+        // 记忆化的处理，memo[n]用赋予了值，就不用继续下面的循环
+        // 直接的返回memo[n] 的最优值
+        if (memo[amount - 1] != 0) {
+            return memo[amount - 1];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+            int res = findWay(coins, amount - coins[i]);
+            if (res >= 0 && res < min) {
+                min = res + 1; // 加1，是为了加上得到res结果的那个步骤中，兑换的一个硬币
             }
         }
-        return minDepth ==Integer.MAX_VALUE?-1:minDepth;
+        memo[amount - 1] = (min == Integer.MAX_VALUE ? -1 : min);
+        return memo[amount - 1];
     }
 
-    private int dfs(int[] coins, int startIndex, int amount, int depth) {
-
-        int newAmount = amount - coins[startIndex];
-        if (newAmount == 0) {
-            return depth + 1;
+    //记忆化搜索逆向 动归
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
         }
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
 
-        if (newAmount<0){
-            if (startIndex == 0){
-                return -1;
+                if (i - coins[j] >= 0) {
+                    int k = dp[i - coins[j]];
+                    if (min > k && k>=0) {
+                        min = k + 1;
+                    }
+                }
             }
-            return dfs(coins,startIndex-1,amount,depth);
+            dp[i] = min == Integer.MAX_VALUE ? -1 : min;
         }
-        int dfs = dfs(coins, startIndex, newAmount, depth + 1);
-        if (dfs<0 && startIndex>0){
-            return dfs(coins,startIndex-1,amount,depth);
-        }
-        return dfs;
+
+        return dp[amount];
     }
+
 }
