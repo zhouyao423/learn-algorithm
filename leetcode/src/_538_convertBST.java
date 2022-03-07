d6ccd4c20a0e5824de38a8dca8b1ecbb3e7e996e
zhouyao <zhouyao423@gmail.com>
2022-03-07 21:22:06 +0800

    leetcode[538]: 累加树 反中序遍历

---
+++ leetcode/src/_538_convertBST.java
@@ -0,0 +1,38 @@
+public class _538_convertBST {
+    //给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+    //
+    //提醒一下，二叉搜索树满足下列约束条件：
+    //
+    //节点的左子树仅包含键 小于 节点键的节点。
+    //节点的右子树仅包含键 大于 节点键的节点。
+    //左右子树也必须是二叉搜索树。
+    //
+    //来源：力扣（LeetCode）
+    //链接：https://leetcode-cn.com/problems/convert-bst-to-greater-tree
+    //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    public TreeNode convertBST(TreeNode root) {
+        dfsSet(root);
+        return root;
+    }
+
+    private void dfsSet(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        dfsSet(root.right);
+        sum +=root.val;
+        root.val = sum;
+        dfsSet(root.right);
+    }
+
+    private int sum = 0;
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        sum += root.val;
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
