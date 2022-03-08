0eec5fcdf288b34200bca7f2cf6a466b95be41fa
zhouyao <zhouyao423@gmail.com>
2022-03-08 18:01:47 +0800

    leetcode[543]: 二叉树深度 dfs

---
+++ leetcode/src/_543_diameterOfBinaryTree.java
@@ -0,0 +1,24 @@
+public class _543_diameterOfBinaryTree {
+    //543. 二叉树的直径
+    //给定一棵二叉树，你需要计算它的直径长度。
+    // 一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        max = Math.max(left + right, max);
+        return Math.max(left, right) + 1;
+    }
+
+    private int max = 0;
+}
